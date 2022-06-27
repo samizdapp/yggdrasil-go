9468af4fb411cfa721e74ce370b89f1158c47642
Ryan Bennett <nomad.ry@gmail.com>
2022-06-27 02:28:16 -0600

    use yg host for homeserver

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -3,11 +3,18 @@
 set -e
 
 CONF_DIR="/etc/yggdrasil-network"
+CONF="$CONF_DIR/config.conf"
 
-if [ ! -f "$CONF_DIR/config.conf" ]; then
-  echo "generate $CONF_DIR/config.conf"
-  yggdrasil --genconf > "$CONF_DIR/config.conf"
+if [ ! -f $CONF ]; then
+  echo "generate $CONF"
+  yggdrasil --genconf -json > "$CONF"
+  tmp=$(mktemp)
+  jq '.NodeInfo = { "samizdapp": { "groups": ["test"] } }' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
+  tmp=$(mktemp)
+  jq '.AdminListen = "tcp://localhost:9001"' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
+  tmp=$(mktemp)
+  # jq '.Peers = ["tls://51.38.64.12:28395"]' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
 fi
 
-yggdrasil --useconf < "$CONF_DIR/config.conf"
+yggdrasil --useconf -json < "$CONF_DIR/config.conf"
 exit $?
