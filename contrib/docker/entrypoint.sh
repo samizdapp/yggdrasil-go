5e9efa23da98dd99e6a0b9af90517bd2bd5a00a1
Ryan Bennett <nomad.ry@gmail.com>
2022-07-06 21:20:27 -0600

    add test and pleroma to entrypoint

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -5,16 +5,16 @@ set -e
 CONF_DIR="/etc/yggdrasil-network"
 CONF="$CONF_DIR/config.conf"
 
-if [ ! -f $CONF ]; then
+# if [ ! -f $CONF ]; then
   echo "generate $CONF"
-  yggdrasil --genconf -json > "$CONF"
+  # yggdrasil --genconf -json > "$CONF"
   tmp=$(mktemp)
-  jq '.NodeInfo = { "samizdapp": { "groups": ["test"] } }' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
+  jq '.NodeInfo = { "samizdapp": { "groups": ["test", "pleroma"] } }' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
   tmp=$(mktemp)
   jq '.AdminListen = "tcp://localhost:9001"' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
   tmp=$(mktemp)
-  # jq '.Peers = ["tls://51.38.64.12:28395"]' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
-fi
+  jq '.Peers = ["tls://51.38.64.12:28395"]' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
+# fi
 
 yggdrasil --useconf -json < "$CONF_DIR/config.conf"
 exit $?
