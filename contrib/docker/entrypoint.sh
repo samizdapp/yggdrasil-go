e376895484e34eac6f7dff1fd2bd9e081d186849
Ryan Bennett <nomad.ry@gmail.com>
2022-07-07 05:23:30 -0600

    refactor mktemp

--- contrib/docker/entrypoint.sh
+++ contrib/docker/entrypoint.sh
@@ -4,17 +4,17 @@ set -e
 
 CONF_DIR="/etc/yggdrasil-network"
 CONF="$CONF_DIR/config.conf"
+tmp=$(mktemp)
 
 if [ ! -f $CONF ]; then
   echo "generate $CONF"
   yggdrasil --genconf -json > "$CONF"
-  tmp=$(mktemp)
   jq '.NodeInfo = { "samizdapp": { "groups": ["test", "pleroma"] } }' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
-  tmp=$(mktemp)
   jq '.AdminListen = "tcp://localhost:9001"' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
-  tmp=$(mktemp)
   jq '.Peers = ["tls://51.38.64.12:28395"]' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
 fi
+
+# jq '.Peers = [  ]' "$CONF" > "$tmp" && mv "$tmp" "$CONF"
 
 yggdrasil --useconf -json < "$CONF_DIR/config.conf"
 exit $?
